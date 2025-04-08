6d1ab906a79e03b86d6645c663c29191a8c94247
Joao Fernando <jf.mattos44@hotmail.com>
2025-04-07 21:25:46 -0300

    Correcoes(html,cass)

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,20 +1,32 @@
 import S from './doados.module.scss'
 import livro from '../../assets/livro.png'
+import { useEffect, useState } from 'react';
 
 
 export default function Doados(){
+
+      const [livros, setLivros] = useState([]);
+    
+      useEffect(() => {
+        fetch('https://desafio2-api.onrender.com/livros')
+          .then(response => response.json())
+          .then(data => setLivros(data))
+          .catch(error => console.error('Erro ao buscar livros:', error));
+      }, []);
+    
     return(
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
             <section className={S.boxcard}>
-                <article>
-                    <img src={livro} alt="imagem de um livro que se chama o protagonista" />
+                {livros.map((livro, index) => (
+                    <article key={index}>
+                        <img src={livro.imagem_url} alt="imagem de um livro" />
                     
-                    <h3>O Protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                    
-                </article>
+                        <h3>{livro.titulo}</h3>
+                        <p>{livro.autor}</p>
+                        <p>{livro.categoria}</p>
+                    </article>
+                ))}
             </section>
         </section>
     )
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,20 +1,45 @@
 import S from './queroDoar.module.scss'
 import livro from '../../assets/Vector.png'
+import { useState } from 'react';
 
 export default function QueroDoar(){
+
+    const[titulo, setTitulo] = useState('');
+    const[categoria, setCategoria] = useState('');
+    const[autor, setAutor] = useState('');
+    const[linkDaImagem, setLinkDaImagem] = useState('');
+
+    function submit(evento){ 
+        evento.preventDefault()
+        const DadosParaApi= {       
+            "titulo": titulo,
+            "categoria": categoria,
+            "autor": autor,
+            "imagem_url": linkDaImagem
+        }
+        fetch('https://desafio2-api.onrender.com/doar',{
+            method:`POST`,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(DadosParaApi)
+        })
+    }
+
     return(
         <section className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action="">
+                <form action="" onSubmit={submit}>
                   <div>
                     <img src={livro} alt="icone de um livro" />
                     <h3>Informações do Livro</h3>
                     </div>  
-                    <input type="text" placeholder='Titulo' />
-                    <input type="text" placeholder='Categoria' />
-                    <input type="text" placeholder='Autor' />
-                    <input type="text" placeholder='Link da Imagem' />
+                    <input value={titulo} onChange={(evento)=>setTitulo(evento.target.value)} type="text" placeholder='Titulo' />
+                    <input value={categoria} onChange={(evento)=>setCategoria(evento.target.value)} type="text" placeholder='Categoria' />
+                    <input value={autor} onChange={(evento)=>setAutor(evento.target.value)} type="text" placeholder='Autor' />
+                    <input valuer={linkDaImagem} onChange={(evento)=>setLinkDaImagem(evento.target.value)} type="text" placeholder='Link da Imagem' />
                     <input type="submit" value="Doar" />
                 </form>
             </section>
